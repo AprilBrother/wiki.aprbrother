e29cdc16a2592fcc3cade9f9d94dfd3c68f9a23c
yanc <volcas@gmail.com>
2024-04-19 15:15:45 +0800

    update vuepress

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,21 +1,8 @@
 const { description } = require('../../package')
-import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
+import { mdEnhancePlugin } from "vuepress-plugin-md-enhance"
+import { defaultTheme } from '@vuepress/theme-default'
 
 module.exports = {
-    locales: {
-        // The key is the path for the locale to be nested under.
-        // As a special case, the default locale can use '/' as its path.
-        '/en/': {
-            lang: 'en-US', // this will be set as the lang attribute on <html>
-            title: 'April Brother Wiki',
-            description: 'April Brother Wiki'
-        },
-        '/zh/': {
-            lang: 'zh-CN',
-            title: 'April Brother Wiki',
-            description: 'April Brother Wiki'
-        }
-    },
     /**
      * Ref：https://v1.vuepress.vuejs.org/config/#title
      */
@@ -40,14 +27,30 @@ module.exports = {
      *
      * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
      */
-    themeConfig: {
-        repo: '',
-        editLinks: false,
-        docsDir: '',
-        editLinkText: '',
-        lastUpdated: false,
+    plugins: [
+        mdEnhancePlugin({
+            // Enable include files
+            include: true,
+            mermaid: true
+        }),
+    ],
+    theme: defaultTheme({
         sidebar: 'auto',
-        nav: [
+        locales: {
+            // The key is the path for the locale to be nested under.
+            // As a special case, the default locale can use '/' as its path.
+            '/en/': {
+                lang: 'en-US', // this will be set as the lang attribute on <html>
+                title: 'April Brother Wiki',
+                description: 'April Brother Wiki'
+            },
+            '/zh/': {
+                lang: 'zh-CN',
+                title: 'April Brother Wiki',
+                description: 'April Brother Wiki'
+            }
+        },
+        navbar: [
             {
                 text: 'Home',
                 link: 'index.md',
@@ -57,12 +60,5 @@ module.exports = {
                 link: 'about.md',
             },
         ]
-    },
-    plugins: [
-        mdEnhancePlugin({
-            // Enable include files
-            include: true,
-            mermaid: true
-        }),
-    ],
+    }),
 }
