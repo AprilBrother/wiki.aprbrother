f66f0874eac648c59fbf1fd7705618751d8959ef
yanc <volcas@gmail.com>
2024-04-19 15:37:07 +0800

    add locales

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -6,26 +6,13 @@ import { webpackBundler } from '@vuepress/bundler-webpack';
 module.exports = {
     title: 'Vuepress Docs Boilerplate',
     bundler: webpackBundler(),
-    /**
-     * Ref：https://v1.vuepress.vuejs.org/config/#description
-     */
     description: description,
-    /**
-     * Extra tags to be injected to the page HTML `<head>`
-     *
-     * ref：https://v1.vuepress.vuejs.org/config/#head
-     */
     head: [
         ['meta', { name: 'theme-color', content: '#3eaf7c' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
         ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
     ],
     shouldPrefetch: () => false,
-    /**
-     * Theme configuration, here is the default theme configuration for VuePress.
-     *
-     * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-     */
     plugins: [
         mdEnhancePlugin({
             // Enable include files
@@ -38,26 +25,20 @@ module.exports = {
         locales: {
             // The key is the path for the locale to be nested under.
             // As a special case, the default locale can use '/' as its path.
-            '/en/': {
-                lang: 'en-US', // this will be set as the lang attribute on <html>
-                title: 'April Brother Wiki',
-                description: 'April Brother Wiki'
+            '/': {
+                selectLanguageName: 'English',
+                navbar: [
+                    {text: 'Home', link: 'index.md'},
+                    {text: 'About', link: 'about.md'},
+                ]
             },
             '/zh/': {
-                lang: 'zh-CN',
-                title: 'April Brother Wiki',
-                description: 'April Brother Wiki'
+                selectLanguageName: '中文',
+                navbar: [
+                    {text: 'Home', link: 'index.md'},
+                    {text: 'About', link: 'about.md'},
+                ]
             }
         },
-        navbar: [
-            {
-                text: 'Home',
-                link: 'index.md',
-            },
-            {
-                text: 'About',
-                link: 'about.md',
-            },
-        ]
     }),
 }
